361c3a9f670d06eca8b99f182bc33f3e88d44924
Diego Moreira de Oliveira <diego.m.oliveira@una.br>
2023-09-28 20:23:34 -0300

    feat: fetch fake data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 const StyledBody = styled.div`
   min-height: 100vh;
@@ -13,11 +14,43 @@ const StyledBody = styled.div`
 const Content = styled.div``;
 
 export default function App() {
+  const [products, setProducts] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setLoading(true);
+        setProducts(null);
+        setError(null);
+
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok) throw new Error(`Error on fetch: ${response.status}`);
+        let data = await response.json();
+
+        setProducts(data);
+        setLoading(false);
+
+        //console.table(data);
+      } catch (error) {
+        setError(error.message);
+        setProducts(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getData();
+  }, []);
+
   return (
     <StyledBody>
       <Content>
         <Header />
-        <Outlet />
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>{error}</h1>}
+        {products && <Outlet context={[products]} />}
       </Content>
       <Footer />
     </StyledBody>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 import Carousel from "../components/Carousel";
+import { useOutletContext } from "react-router-dom";
 
 const StyledSection = styled.section`
   padding: 0 2rem;
 `;
 
 export default function Home() {
+  const [products] = useOutletContext();
+
   return (
     <StyledSection>
       <h1>Welcome to FakeStore!</h1>
