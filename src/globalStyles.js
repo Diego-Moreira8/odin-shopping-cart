84f46d8189666a603cb5ff94f349af3ad2d26455
Diego Moreira de Oliveira <diego.m.oliveira@una.br>
2023-09-28 18:29:38 -0300

    feat: style h1

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -4,6 +4,12 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: Roboto, sans-serif;
   }
+
+  h1 {
+    font: 3rem Rancho, cursive;
+    text-align: center;
+    margin: 2rem;
+  }
 `;
 
 export default GlobalStyles;
